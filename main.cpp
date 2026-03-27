8b5cd89fd77604720f0e50fe3f8187b258cc60f0
EMILY VITORIA FAGUNDES DOS PASSOS <06830785006@senacrs.edu.br>
2026-03-27 20:26:36 -0300

    num2

--- main.cpp
+++ main.cpp
@@ -3,11 +3,13 @@
 
 int main(){
 
+    int num1 = 8;
+    int num2 = 2;
 
-    std::cout<<8+2<<std::endl;
-    std::cout<<10-4<<std::endl;
-    std::cout<<4*3<<std::endl;
-    std::cout<<8/2<<std::endl;
+    std::cout<<num1 + num2<<std::endl;
+    std::cout<<num1 - num2<<std::endl;
+    std::cout<<num1 * num2<<std::endl;
+    std::cout<<num1 / num2<<std::endl;
     
     return 0;
 }
