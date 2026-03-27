d6267ef8d5d351b07e5c6e8352993380c6b67365
EMILY VITORIA FAGUNDES DOS PASSOS <06830785006@senacrs.edu.br>
2026-03-27 20:31:20 -0300

    mult

--- main.cpp
+++ main.cpp
@@ -5,12 +5,18 @@ int main(){
 
     int num1 = 8;
     int num2 = 2;
+    int soma, subt, mult, div;
 
-    std::cout<<num1 + num2<<std::endl;
-    std::cout<<num1 - num2<<std::endl;
-    std::cout<<num1 * num2<<std::endl;
-    std::cout<<num1 / num2<<std::endl;
-    
+    soma = num1 + num2;
+    subt = num1 - num2;
+    mult = num1 * num2;
+    div = num1 / num2;
+
+    std::cout << "Soma: " << soma << std::endl;
+    std::cout << "Subtração: " << subt << std::endl;
+    std::cout << "Multiplicação: " << mult << std::endl;
+    std::cout << "Divisão: " << div << std::endl;
+
     return 0;
 }
 
