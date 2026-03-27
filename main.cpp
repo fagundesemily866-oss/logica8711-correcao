7d86bda04c28565c3d615031dec124d7953ae5b2
EMILY VITORIA FAGUNDES DOS PASSOS <06830785006@senacrs.edu.br>
2026-03-27 20:20:12 -0300

    cout

--- main.cpp
+++ main.cpp
@@ -1,22 +1,14 @@
 #include<iostream>
-#include<iomanip>
+
 
 int main(){
 
-int a = 8;
-//int numero inteiro
-float b = 9.6;
-//float numero de ponto flutuante ocupa 4bytes.
-double c = 4.6666666666;
-std::cout<<std::fixed<<std::setprecision(10)<<c<<std::endl;
 
-//double ocupa 8bytes
-
-std::cout<<"Teste"<<std::endl;
-std::cout<<a<<std::endl;
-std::cout<<b<<std::endl;
-std::cout<<c<<std::endl;
-
+    std::cout<<8+2<<std::endl;
+    std::cout<<10-4<<std::endl;
+    std::cout<<4*3<<std::endl;
+    std::cout<<8/2<<std::endl;
+    
     return 0;
 }
 
