535ad4cdafcb4ee8dc5a54f8097a1e7ed8c28d8d
EMILY VITORIA FAGUNDES DOS PASSOS <06830785006@senacrs.edu.br>
2026-03-27 21:34:47 -0300

    nome

--- main.cpp
+++ main.cpp
@@ -1,16 +1,14 @@
 #include<iostream>
-
+#include<string>
 
 int main(){
+std::string nome;
 
-    int idade = 16;
-    float altura = 1.60;
-    std::string nome = "Emily";
+    std::cout<<"Qual seu nome:"<<std::endl;
+    std::cin>>nome;
 
-    std::cout << "Minha idade é" << idade << std::endl;
-    std::cout << "Minha altura é" << altura << std::endl;
-    std::cout << "Meu nome é" << nome << std::endl;
-
-    return 0;
+    std::cout<<"Bem -vindo(a) "<<nome<<"!!"<<std::endl;
 }
+  return 0;
+
 
