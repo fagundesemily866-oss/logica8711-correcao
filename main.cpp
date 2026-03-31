f8dde50309c20984ac8a91f22e88c7d2bd56250b
EMILY VITORIA FAGUNDES DOS PASSOS <06830785006@senacrs.edu.br>
2026-03-30 21:41:01 -0300

    std

--- main.cpp
+++ main.cpp
@@ -2,12 +2,16 @@
 
 int main(){
 
-int soma1 = 100 + 50;
-int soma2 = soma1 + 50;
-int soma3 = soma2 + 100;
+int a = 10;
 
-std::cout<<soma2<<std::endl;
-std::cout<<soma3<<std::endl;
+std::cout<<++a<<std::endl;
+std::cout<<++a<<std::endl;
+std::cout<<++a<<std::endl;
+
+
+
+
+
 
 return 0;
 }
