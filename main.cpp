764a6332c434eaacbfa4038b2235bea5d6bfe7b2
EMILY VITORIA FAGUNDES DOS PASSOS <06830785006@senacrs.edu.br>
2026-04-06 21:57:59 -0300

    length

--- main.cpp
+++ main.cpp
@@ -11,7 +11,9 @@ std::cout<<nome[2]<<std::endl;
 std::cout<<nome[nome.length()-1]<<std::endl;
 nome[0] = 'A';
 std::cout<<nome<<std::endl;
-
+std::cout<<nome.at(0)<<std::endl;
+nome.at(0) = 'H';
+std::cout<<nome;
 
 
 
