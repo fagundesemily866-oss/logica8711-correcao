8148f2809e2cfeb2d3db48a2c8dd820cf795a92a
EMILY VITORIA FAGUNDES DOS PASSOS <06830785006@senacrs.edu.br>
2026-04-13 19:52:57 -0300

    if else if else

--- main.cpp
+++ main.cpp
@@ -1,20 +1,27 @@
-#include<iostream>
-#include<string>
-
-
+#include <iostream>
 
 int main() {
-
     int idade;
 
-    std::cout << "Digite a sua idade: "<<std::endl;
-    std::cin >> idade;
+    std::cout << "Digite a sua idade: " << std::endl;
 
-    
-    if(idade >= 18) {
-        std::cout << "Voce e maior de idade." << std::endl;
+    if (!(std::cin >> idade)) {
+        std::cout << "Entrada inválida!" << std::endl;
+        return 1;
+    }
+
+    if (idade < 0) {
+        std::cout << "Idade inválida!" << std::endl;
+    } else if (idade <= 2) {
+        std::cout << "Você é um bebê!" << std::endl;
+    } else if (idade <= 8) {
+        std::cout << "Você é uma criança!" << std::endl;
+    } else if (idade <= 16) {
+        std::cout << "Você é um adolescente!" << std::endl;
+    } else if (idade <= 60) {
+        std::cout << "Você é um adulto!" << std::endl;
     } else {
-        std::cout << "Voce e menor de idade." << std::endl;
+        std::cout << "Você é um(a) sênior!" << std::endl;
     }
 
     return 0;
