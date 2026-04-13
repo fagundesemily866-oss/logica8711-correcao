50f6b6880cbfb92a8aa3fd880cb909689a0deecc
EMILY VITORIA FAGUNDES DOS PASSOS <06830785006@senacrs.edu.br>
2026-04-13 20:21:26 -0300

    if else parafuso

--- main.cpp
+++ main.cpp
@@ -3,25 +3,35 @@
 int main() {
     int idade;
 
-    std::cout << "Digite a sua idade: " << std::endl;
+    int opcaoPagamento;
+    int qtdParafuso;
+    float valorParafuso = 1.20;
+    float valortotalparafuso;
+    float desconto;
 
-    if (!(std::cin >> idade)) {
-        std::cout << "Entrada inválida!" << std::endl;
-        return 1;
-    }
+    std::cout << "Digite a quantidade de parafusos desejada: ";
+    std::cin >> qtdParafuso;
 
-    if (idade < 0) {
-        std::cout << "Idade inválida!" << std::endl;
-    } else if (idade <= 2) {
-        std::cout << "Você é um bebê!" << std::endl;
-    } else if (idade <= 8) {
-        std::cout << "Você é uma criança!" << std::endl;
-    } else if (idade <= 16) {
-        std::cout << "Você é um adolescente!" << std::endl;
-    } else if (idade <= 60) {
-        std::cout << "Você é um adulto!" << std::endl;
-    } else {
-        std::cout << "Você é um(a) sênior!" << std::endl;
+    valortotalparafuso = qtdParafuso * valorParafuso;
+
+    std::cout << "digite a quantidade de parafusos desejada: ";
+    std::cin >> qtdParafuso;
+
+    valortotalparafuso = qtdParafuso * valorParafuso;
+
+    std::cout<<" a forma de pagamento e : "<<std::endl;
+    std::cout<<"--- 1 - pix ---"<<std::endl;
+    std::cout<<"--- 2 - dinheiro ---"<<std::endl;
+    std::cout<<"--- 3 - cartao de credito ---"<<std::endl;
+    std::cin>> opcaoPagamento;
+
+    if(opcaoPagamento == 1){
+        std::cout<<"O valor total a ser pago e: "<<valortotalparafuso * 0.95<<std::endl;
+    } else if(opcaoPagamento == 2){
+        desconto = valortotalparafuso * 0.90;
+    std::cout<<"sua compra ficou no valor total de : "<<desconto<<std::endl;
+    } else if(opcaoPagamento == 3){
+        std::cout<<"O valor total a ser pago e: "<<valortotalparafuso<<std::endl;
     }
 
     return 0;
