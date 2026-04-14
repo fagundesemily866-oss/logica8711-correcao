9bd5b988aab5c4b67f76f8d9ba2ca3ece9d4cf6f
EMILY VITORIA FAGUNDES DOS PASSOS <06830785006@senacrs.edu.br>
2026-04-13 21:14:44 -0300

    if else juros

--- main.cpp
+++ main.cpp
@@ -8,30 +8,29 @@ int main() {
     float valorParafuso = 1.20;
     float valortotalparafuso;
     float desconto;
+    float juros;
 
-    std::cout << "Digite a quantidade de parafusos desejada: ";
+    std::cout << "Digite a quantidade de parafusos desejada: " << std::endl;
     std::cin >> qtdParafuso;
 
     valortotalparafuso = qtdParafuso * valorParafuso;
 
-    std::cout << "digite a quantidade de parafusos desejada: ";
-    std::cin >> qtdParafuso;
-
-    valortotalparafuso = qtdParafuso * valorParafuso;
-
-    std::cout<<" a forma de pagamento e : "<<std::endl;
-    std::cout<<"--- 1 - pix ---"<<std::endl;
-    std::cout<<"--- 2 - dinheiro ---"<<std::endl;
-    std::cout<<"--- 3 - cartao de credito ---"<<std::endl;
-    std::cin>> opcaoPagamento;
+    std::cout << " a forma de pagamento e : " << std::endl;
+    std::cout << "--- 1 - pix ---" << std::endl;
+    std::cout << "--- 2 - dinheiro ---" << std::endl;
+    std::cout << "--- 3 - cartao de credito ---" << std::endl;
+    std::cin >> opcaoPagamento;
 
     if(opcaoPagamento == 1){
-        std::cout<<"O valor total a ser pago e: "<<valortotalparafuso * 0.95<<std::endl;
+        std::cout << "O valor total a ser pago e: " << valortotalparafuso * 0.95 << std::endl;
     } else if(opcaoPagamento == 2){
         desconto = valortotalparafuso * 0.90;
-    std::cout<<"sua compra ficou no valor total de : "<<desconto<<std::endl;
+    std::cout << "sua compra ficou no valor total de : " << desconto << std::endl;
     } else if(opcaoPagamento == 3){
-        std::cout<<"O valor total a ser pago e: "<<valortotalparafuso<<std::endl;
+        std::cout << "O valor total a ser pago e: " << valortotalparafuso << std::endl;
+    }else {
+        juros = valortotalparafuso * 0.10;
+        std::cout << "O valor total a ser pago e: " << valortotalparafuso + juros << std::endl;
     }
 
     return 0;
