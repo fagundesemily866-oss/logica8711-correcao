b162717887e8157cf27c6f6b5cbd184f3eb79008
EMILY VITORIA FAGUNDES DOS PASSOS <06830785006@senacrs.edu.br>
2026-04-15 19:48:55 -0300

    forint3

--- main.cpp
+++ main.cpp
@@ -1,12 +1,16 @@
 #include <iostream>
+#include <thread> //para lidar com o tempo de execucao
+#include <chrono> //para definir unidades de tempo (ms, segundos)
 
 int main() {
+
     for(int i = 10; i >= 0; i--){
     std::cout<<"A bomba ira explodir em..."<<i<<std::endl;
+
     }
-    
 
-
+    std::this_thread::sleep_for(std::chrono::milliseconds(500)); 
+ //seconds - > milliseconds
 
 
 
