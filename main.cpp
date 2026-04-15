754a3a71e0a36f1e74884553d9f09bebaf466cfa
EMILY VITORIA FAGUNDES DOS PASSOS <06830785006@senacrs.edu.br>
2026-04-15 19:16:22 -0300

    for int

--- main.cpp
+++ main.cpp
@@ -1,38 +1,25 @@
 #include <iostream>
 
 int main() {
+    for(int i = 0; i < 10; i++){
+    std::cout<<"este e um loop!!"<<std::endl;
+    }
     
 
-std::string nome;
-float altura;
-int idade;
-
-std::cout << "Bem vindo, digite seu nome: ";
-std::cin >> nome;
-std::cout << "Digite sua altura: ";
-std::cin >> altura;
-std::cout << "Digite sua idade: ";
-std::cin >> idade;
-
-std::cout << "Nome: " << nome << std::endl;
-std::cout << "Altura: " << altura << std::endl;
-std::cout << "Idade: " << idade << std::endl;
 
 
-if  (idade >= 10) {
-    std::cout << "Voce se inscreveu na categoria infantil !!" << std::endl;
-}else if (idade >= 18) {
-    std::cout << "Voce se inscreveu na categoria adolescente !!" << std::endl;
-}else if (idade >= 60) {
-    std::cout << "Voce se inscreveu na categoria adulto !!" << std::endl;
 
-std::cout << "Seja muito bem vindo " << nome << " a nossa competicao de natação !!" << std::endl;
+
+
+
+
+
+ return 0;
 
 } 
 
-return 0;
 
-}
+
 
 
  
