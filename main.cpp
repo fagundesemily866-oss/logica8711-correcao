b71816ea60b1507c369ac7e75ab34198614cf44d
EMILY VITORIA FAGUNDES DOS PASSOS <06830785006@senacrs.edu.br>
2026-04-15 20:03:14 -0300

    forintt

--- main.cpp
+++ main.cpp
@@ -7,7 +7,7 @@ int main(){
 int tabuada= 7;
 
 for(int i = 0; i <= 10; i++) {
- std::cout<<"O resultado de "<<tabuada<<" x "<<i<<" = "<<tabuada*i<<std::endl;
+ std::cout<<"O resultado de: "<<tabuada<<" x "<<i<<" = "<<tabuada*i<<std::endl;
 
 }
  return 0;
