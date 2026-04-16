56e497e5e0c699c895fc8035a679464de2f587c3
EMILY VITORIA FAGUNDES DOS PASSOS <06830785006@senacrs.edu.br>
2026-04-15 21:51:01 -0300

    forint

--- main.cpp
+++ main.cpp
@@ -1,19 +1,15 @@
 #include <iostream>
 
 int main(){ 
+ 
+  int mynumbers[5]={10, 20, 30, 40, 50};
+ //for(int num : mynumbers){ 
+//  std::cout<<num<<std::endl;
+for(int i =0; i < 5; ++i){ 
+  std::cout<<mynumbers[i]<<std::endl;
 
-for(int i = 1; i <=2; ++i){
-   std::cout<<"Externo: "<<i<<std::endl;
-   
-for(int j = 1; j <= 3; ++j){
-   std::cout<<"  Interno: "<<j<<std::endl;
    }
     
-
-
-}
-  
-
 return 0;
 }
 
