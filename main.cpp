13546eb523e26bafb7bb537ac6a14390bb2194e8
EMILY VITORIA FAGUNDES DOS PASSOS <06830785006@senacrs.edu.br>
2026-04-28 20:21:27 -0300

    voidifelse

--- main.cpp
+++ main.cpp
@@ -1,13 +1,25 @@
 #include <iostream>
 
- void myFunction(){
-    std::cout<<"Ola mundo!!"<<std::endl;
+ void verificarAprovacao(float nota){
+    if(nota >= 7){
+
+std::cout<<"Status: Aprovado"<<std::endl;
+    }else{
+
+std::cout<<"Status: Reprovado"<<std::endl;
+
+    }
+
 }
-
-int main(){
-
-    myFunction();
+int main() {
     
+float minhaNota = 8.5;
+verificarAprovacao(minhaNota);
+
+
+
+
+
     return 0;
 
 }
