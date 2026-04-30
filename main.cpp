b576d4432d31e0928f449416b360f05ccaccf700
EMILY VITORIA FAGUNDES DOS PASSOS <06830785006@senacrs.edu.br>
2026-04-30 19:22:43 -0300

    ifelse

--- main.cpp
+++ main.cpp
@@ -1,15 +1,31 @@
 #include <iostream>
-#include <string>
-
-int doubleGame(int x){
-    return x * 2;
-}
 
 int main(){
-     for(int i = 1; i <= 5; i++){
-        std::cout << "O dobro de " << i << " é " << doubleGame(i) << std::endl;
 
-     }
+int opcao;
+std::string nome;
+int idade;
+
+std::cout<<"-----Escolha a opcao adequada-----"<<std::endl;
+std::cout<<"Digite 1 para cadastrar ou 2 para sair"<<std::endl;
+std::cin>>opcao;
+
+if(opcao == 1){
+    
+    std::cout<<"Digite seu nome: ";
+    std::cin>>nome;
+    std::cout<<"Digite sua idade: ";
+    std::cin>>idade;
+    std::cout<<"Cadastro realizado com sucesso!"<<std::endl;
+
+std::cout<<"Seu nome e : "<<nome<<std::endl;
+std::cout<<"Sua idade e : "<<idade<<std::endl;
+
+}else{
+
+    std::cout<<"Ate a proxima!!!"<<std::endl;
+}
+     
      
     return 0;
 }
