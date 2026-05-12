a5fd96fe7e1736495a078deec3d3625dac03f922
EMILY VITORIA FAGUNDES DOS PASSOS <06830785006@senacrs.edu.br>
2026-05-12 19:39:38 -0300

    criacao calculadora simples 2.0

--- main.cpp
+++ main.cpp
@@ -4,22 +4,43 @@ int main() {
 
 int num1, num2, soma, subt, mult;
 float div;
+int op;
 
 std::cout<<"Digite aqui o valor para numero 1: ";
 std::cin>>num1;
 
-std::cout<<"Digite o valorv para numero 2: ";
+std::cout<<"Digite o valor para numero 2: ";
 std::cin>>num2;
 
-soma = num1 + num2;
-subt = num1 - num2;
-mult = num1 * num2;
-div = num1 / num2;
+std::cout<<"-------Escolha a operacao -----------"<<std::endl;
+std::cout<<"1. Soma"<<std::endl;
+std::cout<<"2. Subtracao"<<std::endl;
+std::cout<<"3. Multiplicacao"<<std::endl;
+std::cout<<"4. Divisao"<<std::endl;
+std::cin>>op;
 
-std::cout<<"O resultado da soma é: "<<soma<<std::endl;
-std::cout<<"O resultado da subtração é: "<<subt<<std::endl;
-std::cout<<"O resultado da multiplicação é: "<<mult<<std::endl;
-std::cout<<"O resultado da divisão é: "<<div<<std::endl;
+switch (op)
+{
+case 1:
+    soma = num1 + num2;
+    std::cout<<"O resultado da soma é: "<<soma<<std::endl;
+    break;
+case 2:
+    subt = num1 - num2;
+    std::cout<<"O resultado da subtração é: "<<subt<<std::endl;
+    break;
+case 3:
+    mult = num1 * num2;
+    std::cout<<"O resultado da multiplicação é: "<<mult<<std::endl;
+    break;
+case 4:
+    div = num1 / num2;
+    std::cout<<"O resultado da divisão é: "<<div<<std::endl;
+    break;
+}
+
+
+
 
 
     return 0;
