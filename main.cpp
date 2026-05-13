dffe92725cb99dc1060c007c5e5af7c5261a47e1
EMILY VITORIA FAGUNDES DOS PASSOS <06830785006@senacrs.edu.br>
2026-05-12 21:56:00 -0300

    doifelsenumerosorteado

--- main.cpp
+++ main.cpp
@@ -1,74 +1,20 @@
 #include<iostream>
-
-double saldo = 1000.0;
-std::string titular = "Emily";
-
-void exibirSaldo(){ 
-    std::cout<<titular<<"=====Saldo: R$ "<<saldo<<std::endl;
-}
-    void depositar(double valor){ 
-    if (valor > 0){ 
-saldo += valor;
-std::cout<<"Deposito realizado com sucesso!"<<std::endl;
-    }else{ 
- std::cout<<"Valor invalido!"<<std::endl;
-    }
- }
-void sacar(double valor){ 
-     if (valor > 0 && valor <= saldo){ 
-saldo = valor;
-std::cout<<"Saldo insuficiente ou valor invalido!"<<std::endl;
-     }
- }
-
-void exibirMenu(){ 
-    std::cout<<"\n == MENU CONTA BANCARIA == "<<std::endl;
-    std::cout<<"1 = ver saldo"<<std::endl;
-    std::cout<<"2 = depositar"<<std::endl;
-    std::cout<<"3 = sacar"<<std::endl;
-    std::cout<<"4 = sair"<<std::endl;
-    std::cout<<"Digite uma opcao: "<<std::endl;
-
-}
+ 
 int main(){ 
 
-int opcao;
-double valor;
+    int numero, sorteado = 42;
 
-do{ 
+    do{ 
+std::cout<<"Adivinhe: ";
+std::cin>>numero;
+if(numero < sorteado)std::cout<<"Maior!"<<std::endl;
+else if (numero > sorteado)std::cout<<"Menor!"<<std::endl;
+    }while(numero!=sorteado);
 
-    exibirMenu();
-    std::cin>>opcao;
+    std::cout<<"Acertou!"<<std::endl;
 
-    switch (opcao){ 
-    case 1:
-    exibirSaldo();
-    break;
-
-    case 2:
-    std::cout<<"Digite o valor da opcao"<<std::endl;
-    std::cin>>valor;
-    deposito: (valor);
-    break;
-
-    case 3:
-    std::cout<<"Digite o valor a sacar"<<std::endl;
-    std::cin>>valor;
-    sacar(valor);
-    break;
-
-    case 4:
-    std::cout<<"Saindo da conta..."<<std::endl;
-    break;
-    default :
-    std::cout<<"Opcao invalida!"<<std::endl;
+    return 0;
 }
-}while(opcao != 4);
-
-return 0;
-}
-
-
 
 
 
