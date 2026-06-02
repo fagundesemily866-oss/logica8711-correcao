520f456767875b2cf7a8c704144e5f7363cc152b
EMILY VITORIA FAGUNDES DOS PASSOS <06830785006@senacrs.edu.br>
2026-06-02 20:51:24 -0300

    ponteiro

--- main.cpp
+++ main.cpp
@@ -1,37 +1,13 @@
-#include<cctype>
 #include<iostream>
-#include<string>
-
-bool ehPalindromo(std::string s, int inicio, int fim) {
-    while (inicio < fim){
-        if (inicio >= fim){
-            return true;
-        }
-        if(s[inicio] != s[fim]){
-            return false;
-        }
-        return ehPalindromo(s, inicio + 1, fim - 1);
-    }
-}
-
 
 int main(){
 
-    std::string palavra;
+ int numero = 42;
+ int* ptr = &numero;
 
-    std::cout << "Digite uma palavra: ";
-    std::cin >> palavra;
+std::cout<<"Acesar via variavel: "<<numero<<std::endl;
+std::cout<<"Acesar via ponteiro: "<<*ptr<<std::endl;
 
-    for (char &c : palavra) {
-        c = std::tolower(c);
-    }
-
-    if(ehPalindromo(palavra, 0, palavra.length() - 1)){
-        std::cout << "A palavra é um palíndromo." << std::endl;
-    }else{       
-        std::cout << "A palavra não é um palíndromo." << std::endl;
-    }
 
     return 0;
 }
-
