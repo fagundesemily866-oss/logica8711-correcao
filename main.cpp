185660be6c3b13efc6baabf289e2f206036aca07
EMILY VITORIA FAGUNDES DOS PASSOS <06830785006@senacrs.edu.br>
2026-06-02 20:13:15 -0300

    palindromo

--- main.cpp
+++ main.cpp
@@ -2,32 +2,35 @@
 #include<iostream>
 #include<string>
 
-bool ehPalindromo(std::string str){
-    int inicio = 0;
-    int fim = str.length() - 1;
-
-    while(inicio < fim){
-        if(str[inicio] != str[fim]){
+bool ehPalindromo(std::string s, int inicio, int fim) {
+    while (inicio < fim){
+        if (inicio >= fim){
+            return true;
+        }
+        if(s[inicio] != s[fim]){
             return false;
         }
-        inicio++;
-        fim--;
+        return ehPalindromo(s, inicio + 1, fim - 1);
     }
-
-    return true;
 }
 
-int main() {
+
+int main(){
 
     std::string palavra;
 
     std::cout << "Digite uma palavra: ";
     std::cin >> palavra;
 
-    for(int i = 0; i < palavra.length(); i++){
-        palavra[i] = std::tolower(palavra[i]);
+    for (char &c : palavra) {
+        c = std::tolower(c);
     }
-    std::cout << ehPalindromo(palavra) << std::endl;
+
+    if(ehPalindromo(palavra, 0, palavra.length() - 1)){
+        std::cout << "A palavra é um palíndromo." << std::endl;
+    }else{       
+        std::cout << "A palavra não é um palíndromo." << std::endl;
+    }
 
     return 0;
 }
