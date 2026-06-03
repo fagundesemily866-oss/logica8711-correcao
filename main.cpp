e7f7ed777f49a9c13476eb8073caab5c8f9d99e1
EMILY VITORIA FAGUNDES DOS PASSOS <06830785006@senacrs.edu.br>
2026-06-02 21:08:07 -0300

    adiciona struct Pessoa

--- main.cpp
+++ main.cpp
@@ -1,16 +1,22 @@
 #include<iostream>
+#include<string>
 
-void dividir(int a, int b, int* quociente, int* resto){
-    *quociente = a / b;
-    *resto = a % b;
-}
+struct Pessoa{
+    std::string nome;
+    int idade;
+    float altura;
+};
 
 int main(){
 
-   int q, r;
-   dividir(17, 5, &q, &r);
-   std::cout<<"Quociente: "<<q<<",Resto: "<<r<<std::endl;
+   Pessoa p1;
+   p1.nome = "emily";
+   p1.idade = 16;
+   p1.altura = 1.65;
 
+std::cout<<"Nome: "<<p1.nome<<std::endl;
+std::cout<<"Idade: "<<p1.idade<<std::endl;
+std::cout<<"Altura: "<<p1.altura<<std::endl;
 
 
     return 0;
