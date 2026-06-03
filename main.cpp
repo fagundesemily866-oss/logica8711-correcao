2f3c6fb384200440806cee2500b9c01144d07634
EMILY VITORIA FAGUNDES DOS PASSOS <06830785006@senacrs.edu.br>
2026-06-02 21:15:40 -0300

    exercicio de structs com multiplos objetos

--- main.cpp
+++ main.cpp
@@ -2,6 +2,8 @@
 #include<string>
 
 struct Pessoa{
+
+
     std::string nome;
     int idade;
     float altura;
@@ -9,14 +11,12 @@ struct Pessoa{
 
 int main(){
 
-   Pessoa p1;
-   p1.nome = "emily";
-   p1.idade = 16;
-   p1.altura = 1.65;
+Pessoa p1 = {"Carla", 20, 1.55};
+Pessoa p2 = {"Joao", 25, 1.80};
 
-std::cout<<"Nome: "<<p1.nome<<std::endl;
-std::cout<<"Idade: "<<p1.idade<<std::endl;
-std::cout<<"Altura: "<<p1.altura<<std::endl;
+
+std::cout<<p1.nome<<" tem "<<p1.idade<<" anos "<<" e mede "<<p1.altura<<"m"<<std::endl;
+std::cout<<p2.nome<<" tem "<<p2.idade<<" anos "<<" e mede "<<p2.altura<<"m"<<std::endl;
 
 
     return 0;
