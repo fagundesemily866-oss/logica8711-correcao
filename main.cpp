ce2a8487ac95f570828bc756937935fd50454a1e
EMILY VITORIA FAGUNDES DOS PASSOS <06830785006@senacrs.edu.br>
2026-06-09 21:54:05 -0300

    exemplo para simplificar e diminuir o codigo

---
+++ main.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+
+int main(){
+    int idade = 16;
+
+    std::string resultado = (idade >= 18) ? "Maior de idade" : "Menor de idade";
+
+    std::cout<<resultado<<std::endl;
+
+}
+
+
+    
+        
+    
+    
