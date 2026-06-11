31609cce482588e4cc200b1244f2aa1d44eb1449
EMILY VITORIA FAGUNDES DOS PASSOS <06830785006@senacrs.edu.br>
2026-06-11 19:32:47 -0300

    torre de hanoi

--- main.cpp
+++ main.cpp
@@ -1,13 +1,26 @@
 #include<iostream>
+#include<string>
 
-int main(){
-    int idade = 16;
-
-    std::string resultado = (idade >= 18) ? "Maior de idade" : "Menor de idade";
-
-    std::cout<<resultado<<std::endl;
-
+void torreDeHanoi(int n, std::string origem, std::string destino, std::string auxiliar) {
+    if (n == 1) {
+        std::cout << "Mover disco 1 de " << origem << " para " << destino << std::endl;
+        return;
+    }
+    torreDeHanoi(n - 1, origem, auxiliar, destino);
+    std::cout << "Mover disco " << n << " de " << origem << " para " << destino << std::endl;
+    torreDeHanoi(n - 1, auxiliar, destino, origem);
 }
+int main(){
+    int numDiscos;
+    std::cout << "Digite o número de discos: ";
+    std::cin >> numDiscos;
+
+    std::cout<<"\nSequencia de movimentos: "<<std::endl;
+    torreDeHanoi(numDiscos, "A", "C", "B");
+
+    std::cout<<"\nTotal de movimento: "<<(1 << numDiscos) - 1 << std::endl; 
+}
+
 
 
     
