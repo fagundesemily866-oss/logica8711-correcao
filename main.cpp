bc50cb42fa46bcd7e02c39c42034a897431062c4
EMILY VITORIA FAGUNDES DOS PASSOS <06830785006@senacrs.edu.br>
2026-06-11 21:11:57 -0300

    algoritimo de horner

--- main.cpp
+++ main.cpp
@@ -1,16 +1,14 @@
-#include <iostream>
-
-int encontrarMaior(int* arr, int tamanho){
-    int maior = arr[0];
-    for(int i = 1; i < tamanho; i++){
-        if(arr[i] > maior){
-            maior = arr[i];
-        }
+#include<iostream>
+//algoritimo de horner
+int horner(int* coeficientes, int grau, int x){
+    int resultado = coeficientes[grau];
+     for(int i = grau - 1; i >= 0; i--){
+        resultado = resultado * x + coeficientes[i];
+     }
+     return resultado;
     }
-return maior;
-}
 int main(){
-    int arr[] = {3, 7, 2, 9, 1, 5};
-    std::cout<<"Maior elemento: "<<encontrarMaior(arr, 6)<<std::endl;
-    return 0;
+    int coef[] = {5, 4, 3, 2};
+std::cout<<"p(2) = "<<horner(coef, 3, 2)<<std::endl;
+return 0;
 }
