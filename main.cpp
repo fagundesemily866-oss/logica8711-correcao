fb138636caee9c0e6adf94c406b0517feb6c34ae
EMILY VITORIA FAGUNDES DOS PASSOS <06830785006@senacrs.edu.br>
2026-06-17 21:36:49 -0300

    Registro de cartoes

--- main.cpp
+++ main.cpp
@@ -1,29 +1,41 @@
 #include<iostream>
 #include<string>
 
-struct CampeonatoCopa{
-    int ano;
-    std::string campeao;
+struct Jogador{
+    std::string nome;
+    int cartaoAmarelo;
+    int cartaoVermelho;
 };
 
 int main(){
-CampeonatoCopa copa[5] = {
-    {2022, "Argentina"},
-    {2018, "Franca"},
-    {2014, "Alemanha"},
-    {2010, "Espanha"},
-    {2006, "Italia"},
-};
-
-
-std::cout<<"========Campeoes da Copa========"<<std::endl;
+std::cout<<"==========REGISTRO DE CARTOES======="<<std::endl;
 std::cout<<std::endl;
 
-for(int i = 0; i < 5; i++){
-    std::cout<<copa[i].ano<<" - Campeao: "<<copa[i].campeao<<std::endl;
+Jogador jogador;
+
+std::cout<<"Nome do jogador: "<<std::endl;
+std::cin>>jogador.nome;
+
+std::cout<<"Cartoes amarelos: "<<std::endl;
+std::cin>>jogador.cartaoAmarelo;
+
+std::cout<<"Cartoes vermelhos: "<<std::endl;
+std::cin>>jogador.cartaoVermelho;
+
+std::cout<<std::endl;
+std::cout<<"===========SITUACAO======"<<std::endl;
+
+if(jogador.cartaoVermelho > 0){
+    std::cout<<jogador.nome<<" foi expulso!"<<std::endl;
+}else if(jogador.cartaoAmarelo >= 2){
+    std::cout<<jogador.cartaoAmarelo<<"recebeu 2 amarelos e foi expulso!"<<std::endl;
+}else if(jogador.cartaoAmarelo == 1){ 
+std::cout<<jogador.nome<<" recebeu 1 amarelo. Cuidado!"<<std::endl;
+}else{ 
+    std::cout<<jogador.nome<<" esta limpo! "<<std::endl;
 }
- return 0;
 }
+
 
 
 
