98b39fb52a466bfe60e3fb72e2ae17cd204fa7c5
EMILY VITORIA FAGUNDES DOS PASSOS <06830785006@senacrs.edu.br>
2026-06-23 21:28:38 -0300

    min e max em matriz

--- main.cpp
+++ main.cpp
@@ -1,39 +1,8 @@
-#include<iostream>
 
-int main(){
- 
-    int numeros[] {10, 20, 30, 40, 50, 60, 70, 80};
-    int tamanho = 8;
-    int buscado;
 
-    std::cout<<"==== BUSCAR NUMERO ===="<<std::endl;
-    std::cout<<std::endl;
 
-    std::cout<<"Array: ";
-    for(int i = 0; i < tamanho; i++){
-        std::cout<<numeros[i]<<" ";
-    }
-    std::cout<<std::endl;
 
-    std::cout<<"Digite o numeros a buscar: ";
-    std::cin>>buscado;
 
-    int posicao = -1;
-
-    for(int i = 0; i < tamanho; i++){
-        if(numeros[i] == buscado){
-            posicao = i;
-            break;
-        }
-    }
-    std::cout<<std::endl;
-    if(posicao != -1){
-        std::cout<<"Encontrado na posicao "<<std::endl;
-    }else{
-        std::cout<<"Nao encontrado!"<<std::endl;
-    }
-    return 0;
-}
 
 
 
@@ -49,7 +18,40 @@ int main(){
 
 
 
+#include<iostream>
 
+int main(){
+
+
+    int matriz[3][4] = {
+        {10, 25, 5, 40},
+        {15, 8, 30, 12},
+        {20, 3, 18, 35}
+    };
+
+    int maior = matriz[0][0];
+    int menor = matriz[0][0];
+    int linhaM = 0, colunaM = 0;
+    int linhaN = 0, colunaN = 0;
+
+    std::cout<<"==== MIN E MAX EM MATRIZ"<<std::endl;
+
+    std::cout<<"Matriz: "<<std::endl;
+    for(int i = 0; i < 3; i++){
+        for(int j = 0; j < 4; j++){
+            std::cout<<matriz[i][j]<<" ";
+
+            if(matriz[i][j] > maior){
+                maior = matriz[i][j];
+                linhaM = i;
+                colunaM = j;
+            }
+        }
+    }
+std::cout<<std::endl;
+std::cout<<"Maior: "<<maior<<"na posicao["<<linhaM<<"]["<<colunaM<<"]"<<std::endl;
+std::cout<<"Menor: "<<menor<<"na posicao["<<linhaN<<"]["<<colunaN<<"]"<<std::endl;
+}
 
 
 
