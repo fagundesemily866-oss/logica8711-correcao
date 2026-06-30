24f401c6fd242b500bd53a30e807ac35e51357dd
EMILY VITORIA FAGUNDES DOS PASSOS <06830785006@senacrs.edu.br>
2026-06-30 19:36:29 -0300

    exercico 1 - Ler e exibir

--- main.cpp
+++ main.cpp
@@ -1,57 +1,20 @@
-
-
-
-
-
-
-
-
-
-
-
-
-   
-
-
-
-
-
-
-
-#include<iostream>
+#include <iostream>
 
 int main(){
+    int numeros[6] = {10, 20, 30, 40, 50, 60};
+    int tamanho = sizeof(numeros) / sizeof(numeros[0]);
 
+    std::cout << "========= 1. EXIBIR VALORES PREDEFINIDOS ==========" << std::endl;
+    std::cout << "Numeros armazenados" << std::endl;
 
-    int matriz[3][4] = {
-        {10, 25, 5, 40},
-        {15, 8, 30, 12},
-        {20, 3, 18, 35}
-    };
-
-    int maior = matriz[0][0];
-    int menor = matriz[0][0];
-    int linhaM = 0, colunaM = 0;
-    int linhaN = 0, colunaN = 0;
-
-    std::cout<<"==== MIN E MAX EM MATRIZ"<<std::endl;
-
-    std::cout<<"Matriz: "<<std::endl;
-    for(int i = 0; i < 3; i++){
-        for(int j = 0; j < 4; j++){
-            std::cout<<matriz[i][j]<<" ";
-
-            if(matriz[i][j] > maior){
-                maior = matriz[i][j];
-                linhaM = i;
-                colunaM = j;
-            }
-        }
+    for(int i = 0; i < tamanho; i++){
+        std::cout << numeros[i] << " ";
     }
-std::cout<<std::endl;
-std::cout<<"Maior: "<<maior<<"na posicao["<<linhaM<<"]["<<colunaM<<"]"<<std::endl;
-std::cout<<"Menor: "<<menor<<"na posicao["<<linhaN<<"]["<<colunaN<<"]"<<std::endl;
+    std::cout << std::endl;
+
+    return 0;
 }
+
 
 
 
