ada4c396e0453ad928dfa04125445be49e88efa8
EMILY VITORIA FAGUNDES DOS PASSOS <06830785006@senacrs.edu.br>
2026-06-30 20:23:57 -0300

    5. CALCULAR MEDIA

--- main.cpp
+++ main.cpp
@@ -3,23 +3,26 @@
 int main(){
 
     int numeros[5];
-    int menor = 0;
+    float soma = 0;
+    float media = 0;
 
-    std::cout << "========= 3. ENCONTRAR MENOR ==========" << std::endl;
+    std::cout << "========= 5. CALCULAR MEDIA ==========" << std::endl;
 
     for(int i = 0; i < 5; i++){
         std::cout << "Digite o numero " << (i + 1) << ": ";
         std::cin >> numeros[i];
+        soma += numeros[i];
     }
+    media = soma / 5;
 
-    menor = numeros[0];
-    for(int i = 1; i < 5; i++){
-        if(numeros[i] < menor){
-            menor = numeros[i];
-        }
+    std::cout << "Numeros digitados: ";
+    for(int i = 0; i < 5; i++){
+        std::cout << numeros[i] << " ";
     }
+    std::cout << std::endl;
 
-    std::cout << "O menor numero digitado foi: " << menor << std::endl;
+    std::cout << "Soma: " << soma << std::endl;
+    std::cout << "Media: " << media << std::endl;
 
     return 0;
 }
