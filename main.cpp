4bce71b9b91b73afa984466954e5c7c4eb34a093
EMILY VITORIA FAGUNDES DOS PASSOS <06830785006@senacrs.edu.br>
2026-06-30 19:49:00 -0300

    exercic .2 - Soma simples

--- main.cpp
+++ main.cpp
@@ -3,24 +3,24 @@
 int main(){
 
     int numeros[5];
-
-    std::cout << "========= 1. LER E EXIBIR ==========" << std::endl;
+    int soma = 0;
+    std::cout << "========= 2. SOMA SIMPLES ==========" << std::endl;
 
     // Leitura dos valores
     for(int i = 0; i < 5; i++){
         std::cout << "Digite o numero " << (i + 1) << ": ";
         std::cin >> numeros[i];
+        soma += numeros[i];
     }
 
     // Exibição dos valores
-    std::cout << "Valores digitados: ";
-    for(int i = 0; i < 5; i++){
+    std::cout << "Soma total: ";
         std::cout << numeros[i] << " ";
     }
     std::cout << std::endl;
 
     return 0;
-}
+
 
 
 
