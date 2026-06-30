9eefb1693f8da674d71c80cd08a806c6c9a7a8f0
EMILY VITORIA FAGUNDES DOS PASSOS <06830785006@senacrs.edu.br>
2026-06-30 20:00:09 -0300

    3. ENCONTRAR MAIOR

--- main.cpp
+++ main.cpp
@@ -3,24 +3,24 @@
 int main(){
 
     int numeros[5];
-    int soma = 0;
-    std::cout << "========= 2. SOMA SIMPLES ==========" << std::endl;
+    int maior = 0;
 
-    // Leitura dos valores
+    std::cout << "========= 3. ENCONTRAR MAIOR ==========" << std::endl;
+
     for(int i = 0; i < 5; i++){
         std::cout << "Digite o numero " << (i + 1) << ": ";
         std::cin >> numeros[i];
-        soma += numeros[i];
     }
-
-    // Exibição dos valores
-    std::cout << "Soma total: ";
-        std::cout << numeros[i] << " ";
+    maior = numeros[0];
+    for(int i = 0; i < 5; i++){
+        if(numeros[i] > maior){
+            maior = numeros[i];
+        }
     }
     std::cout << std::endl;
-
+    std::cout<<" o maior numero digitado foi: "<<maior<<std::endl;
     return 0;
-
+}
 
 
 
