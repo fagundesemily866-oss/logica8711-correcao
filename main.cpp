2cd06494c6a9231d2651195e2d94484e6a15aa42
EMILY VITORIA FAGUNDES DOS PASSOS <06830785006@senacrs.edu.br>
2026-06-30 20:09:14 -0300

    4. ENCONTRAR MENOR

--- main.cpp
+++ main.cpp
@@ -1,27 +1,28 @@
- #include <iostream>
+#include <iostream>
 
 int main(){
 
     int numeros[5];
-    int maior = 0;
+    int menor = 0;
 
-    std::cout << "========= 3. ENCONTRAR MAIOR ==========" << std::endl;
+    std::cout << "========= 3. ENCONTRAR MENOR ==========" << std::endl;
 
     for(int i = 0; i < 5; i++){
         std::cout << "Digite o numero " << (i + 1) << ": ";
         std::cin >> numeros[i];
     }
-    maior = numeros[0];
-    for(int i = 0; i < 5; i++){
-        if(numeros[i] > maior){
-            maior = numeros[i];
+
+    menor = numeros[0];
+    for(int i = 1; i < 5; i++){
+        if(numeros[i] < menor){
+            menor = numeros[i];
         }
     }
-    std::cout << std::endl;
-    std::cout<<" o maior numero digitado foi: "<<maior<<std::endl;
+
+    std::cout << "O menor numero digitado foi: " << menor << std::endl;
+
     return 0;
 }
-
 
 
 
