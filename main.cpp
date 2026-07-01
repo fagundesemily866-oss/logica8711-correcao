b836fcec17ca6ec78a23f759127a2fdbbf0d49e1
EMILY VITORIA FAGUNDES DOS PASSOS <06830785006@senacrs.edu.br>
2026-06-30 21:33:27 -0300

    7.8 EXIBIR PARES E IMPARES

--- main.cpp
+++ main.cpp
@@ -2,39 +2,30 @@
 
 int main(){
 
-int numeros [] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
-int tamanho = 10;
-int buscado;
+    int numeros[] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+    int tamanho = 10;
 
-    std::cout << "========= 6. CONTAR ELEMENTOS ==========" << std::endl;
-    std::cout<<std::endl; 
+    std::cout << "========= 7.8 EXIBIR PARES E IMPARES ==========" << std::endl;
+    std::cout << std::endl; 
 
-    std::cout<<"====== TOTAL DE ELEMENTOS ======="<<std::endl;
-
-    std::cout<<"Array: ";
-    for(int i = 0; i < tamanho; i++){ 
-        std::cout<<numeros[i]<<" ";
-    }
-     std::cout<<std::endl; 
-
-    std::cout<<"NUMERO A BUSCAR"<<std::endl;
-    std::cin>>buscado;
-
-    int posicao = -1;
+    std::cout << "Pares: ";
     for(int i = 0; i < tamanho; i++){
-          if(numeros[i] == buscado){
-            posicao = i;
-            break;
+        if(numeros[i] % 2 == 0){
+            std::cout << numeros[i] << " ";
         }
     }
-       std::cout<<std::endl;
-    if(posicao != -1){
-        std::cout<<"Encontrado na posicao "<<std::endl;
-    }else{
-        std::cout<<"Nao encontrado!"<<std::endl;
+    std::cout << std::endl;
+
+    std::cout << "Impares: ";
+    for(int i = 0; i < tamanho; i++){
+        if(numeros[i] % 2 != 0){
+            std::cout << numeros[i] << " ";
+        }
     }
+    std::cout << std::endl;
+
     return 0;
-       }
+}
 
 
 
